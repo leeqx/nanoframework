e1e30c06c5550af2b35836f0d40e1d8ca0e1dc1c
李青鑫 <liqingxin@xunlei.com>
2016-01-25 09:34:55 +0800

    初始化静态变量

--- msghandler/handler_factory.cpp
+++ msghandler/handler_factory.cpp
@@ -1,3 +1,4 @@
 #include "handler_factory.h"
 
-pthread_mutex_t CNanoFactory::m_mutex=PTHREAD_MUTEX_INITIALIZER;
+CNanoFactory *CNanoFactory::m_pNanoFactory = NULL;
+pthread_mutex_t CNanoFactory::m_mutex      = PTHREAD_MUTEX_INITIALIZER;
