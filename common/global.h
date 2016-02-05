444d6253490cc0e1764a178d61c6829b2d23930d
李青鑫 <liqingxin@xunlei.com>
2016-02-05 16:26:53 +0800

    修复宏定义变量;引发的重定义问题

--- common/global.h
+++ common/global.h
@@ -12,24 +12,24 @@
 #define    INFO 1
 #define    WARN 2
 #define    MAX_EVENTS_SIZE  65535
+#define    time(x) 0
 
 #define __LOG(fd,level,fmt,...) \
-    long int cur=0; \
     if((level) == ERROR)\
     { \
-        fprintf(fd,"%ld,ERROR:%s-%s-%d:" fmt,cur,__FILE__,__func__,__LINE__,##__VA_ARGS__);\
+        fprintf(fd,"%ld,ERROR:%s-%s-%u:" fmt "\n",time(NULL),__FILE__,__func__,__LINE__,##__VA_ARGS__);\
     } \
     else if ((level) == DEBUG) \
     { \
-        fprintf(fd,"%ld,DEBUG:%s-%s-%d:" fmt,cur,__FILE__,__func__,__LINE__,##__VA_ARGS__);\
+        fprintf(fd,"%ld,DEBUG:%s-%s-%u:" fmt "\n",time(NULL),__FILE__,__func__,__LINE__,##__VA_ARGS__);\
     }\
     else if((level) == INFO) \
     { \
-        fprintf(fd,"%ld,INFO:%s-%s-%d:" fmt,cur,__FILE__,__func__,__LINE__,##__VA_ARGS__);\
+        fprintf(fd,"%ld,INFO:%s-%s-%u:" fmt "\n",time(NULL),__FILE__,__func__,__LINE__,##__VA_ARGS__);\
     }\
     else if((level) == WARN) \
     { \
-        fprintf(fd,"%ld,INFO:%s-%s-%d:" fmt,cur,__FILE__,__func__,__LINE__,##__VA_ARGS__);\
+        fprintf(fd,"%ld,INFO:%s-%s-%u:" fmt "\n",time(NULL),__FILE__,__func__,__LINE__,##__VA_ARGS__);\
     }
 
 #define LOG(level,fmt,...) __LOG(stdout,level,fmt,##__VA_ARGS__)
